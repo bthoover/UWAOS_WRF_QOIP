d224c3d4396424407892e923fa0490a03138e16a
Brett Hoover <brett.hoover@ssec.wisc.edu>
2023-06-11 17:37:34 -0500

    Added gen_wrf_proj() and gen_cartopy_proj() to analysis/analysis_dependencies.py; began analysis/final_run/ subdirectory with example plotting .ipynb

--- analysis/analysis_dependencies.py
+++ analysis/analysis_dependencies.py
@@ -94,4 +94,81 @@ def get_wrf_rh(wrfHdl):
     rh = wrf.rh(qVap, P, T)  # (%)
     # Return rh
     return rh
+# gen_wrf_proj: generate a wrf.WrfProj() object from metadata contained in a WRF file
+#
+# INPUTS:
+#
+# wrfHDL: WRF file netCDF4.Dataset() handle
+#
+# OUTPUTS:
+#
+# wrfProjObj: wrf.WrfProj() object containing WRF grid's map projection properties.
+#
+# DEPENDENCIES:
+#
+# netCDF4.Dataset()
+# wrf.WrfProj()
+def gen_wrf_proj(wrfHdl):
+    from netCDF4 import Dataset
+    from wrf import WrfProj
+    
+    return WrfProj(map_proj=wrfHdl.MAP_PROJ,
+                   truelat1=wrfHdl.TRUELAT1,
+                   truelat2=wrfHdl.TRUELAT2,
+                   moad_cen_lat=wrfHdl.MOAD_CEN_LAT,
+                   stand_lon=wrfHdl.STAND_LON,
+                   pole_lat=wrfHdl.POLE_LAT,
+                   pole_lon=wrfHdl.POLE_LON,
+                   dx=wrfHdl.DX,
+                   dy=wrfHdl.DY
+                  )
+# gen_cartopy_proj: generate a cartopy.crs() object from metadata contained in a WRF file
+#
+# INPUTS:
+#
+# wrfHDL: WRF file netCDF4.Dataset() handle
+#
+# OUTPUTS:
+#
+# crsProjObj: cartopy.crs() object containing WRF grid's map projection properties
+#
+# DEPENDENCIES:
+#
+# netCDF4.Dataset()
+# cartopy.crs()
+#
+# NOTES:
+# I am following the advice given in this post to explicitly define an Earth SPHERE for
+# the projection by defining the cartopy.crs.Globe() with a semimajor_axis and
+# semiminor_axis of 6370000. m to match WRF's definition of the Earth as a sphere
+# rather than as an ellipsoid.
+# https://fabienmaussion.info/2018/01/06/wrf-projection/
+# I don't see any significant difference with/without the explicit definition of an
+# Earth sphere (lat/lon pass back and forth between either projection and
+# cartopy.crs.PlateCarree() with differences on the order of 1.0e-9 degrees), but
+# since this is apparently the correct way to do it and it doesn't generate large
+# differences, I'm going to go with it in-case my proj <--> PlateCarree tests are
+# being done incorrectly. I'm doing no harm with it, as far as I can tell.
+def gen_cartopy_proj(wrfHDL):
+    from netCDF4 import Dataset
+    from cartopy import crs as ccrs
+    # WRF is assumed on a Lambert Conformal projection, if it's not the routine
+    # will report an error and return None. You can add more projection options
+    # as you run into them, but the most common projection is Lambert Conformal
+    # so I'm only coding that one here.
+    if (wrfHDL.MAP_PROJ == 1) & (wrfHDL.MAP_PROJ_CHAR == "Lambert Conformal"):
+        return ccrs.LambertConformal(
+                                     central_longitude=wrfHDL.CEN_LON,
+                                     central_latitude=wrfHDL.CEN_LAT,
+                                     standard_parallels=(wrfHDL.TRUELAT1,wrfHDL.TRUELAT2),
+                                     globe=ccrs.Globe(
+                                                      semimajor_axis=6370000.,
+                                                      semiminor_axis=6370000.
+                                                     )
+                                    )
+    else:
+        print('FAILED: UNKNOWN PROJECTION ENCOUNTERED:')
+        print('.MAP_PROJ={:d}'.format(wrfHDL.MAP_PROJ))
+        print('.MAP_PROJ_CHAR=' + wrfHDL.MAP_PROJ_CHAR)
+        return None
 
