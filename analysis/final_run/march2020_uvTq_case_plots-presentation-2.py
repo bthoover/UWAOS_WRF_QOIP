816ee4fbb1f7262e44ecf31792cd8278f1f86fa3
Brett Hoover <brett.hoover@ssec.wisc.edu>
2024-01-02 16:22:28 -0600

    Added more energy profile stuff to analysis/final_run/march2020_uvTq_case_plots-presentation-2.py

--- analysis/final_run/march2020_uvTq_case_plots-presentation-2.py
+++ analysis/final_run/march2020_uvTq_case_plots-presentation-2.py
@@ -2699,6 +2699,132 @@ if __name__ == "__main__":
     #
 
 
+# In[66]:
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+from netCDF4 import Dataset
+import glob
+import matplotlib.cm as cm
+# define local functions
+#
+# plot_normalized_iterprofiles: generates a plot of normalized profiles (sums to 1.0 for each profile)
+# for each iteration on a given colormap / plot axis
+#
+# INPUTS
+#
+# profVar (float): profile variable in (nLev,nIter) dimension
+# profLev (float): profile levels in (nLev,) dimension
+# titleStr (string): title for plot
+# colMap (string): name of colormap
+# ax (plt.axes): matplotlib.pyplot axis
+#
+# OUTPUTS
+#
+# no explicit outputs, but places figure on axis=ax
+#
+# DEPENDENCIES
+#
+# matplotlib.pyplot
+# matplotlib.cm
+# numpy
+def plot_normalized_iterprofiles(profVar, profLev, titleStr, colMap, ax):
+    # define number of levels (nl) and number of iterations (ni) based on
+    # implicit dimensions of profVar [nl,ni]
+    nl,ni = np.shape(profVar)
+    # define colormap for each profile based on a colormap of colMap with
+    # a length of [0,ni-1]
+    scalarMap = cm.ScalarMappable(cmap=colMap)
+    scalarMapList = scalarMap.to_rgba(range(ni))
+    # loop through iterations
+    for i in range(ni):
+        # define profile color as ith element of scalarMapList
+        profColor = list(scalarMapList[i][0:3])
+        # define profile as ith iteration's profile [nl,]
+        prof = profVar[:,i].squeeze()
+        # plot normalized profile
+        ax.plot(prof/np.abs(np.nansum(prof)),profLev,color=profColor,linewidth=2)
+        #ax.plot(prof,profLev,color=profColor,linewidth=2)
+    # plot a dashed zero-line profile for reference
+    ax.plot(np.zeros((nl,)),profLev,color='black',linestyle='dashed',linewidth=2)
+    # add title
+    ax.set_title(titleStr)
+    # plotting on eta-levels: reverse y-axis
+    ax.invert_yaxis()
+    return
+#
+# begin
+#
+if __name__ == "__main__":
+    cp = 1004.
+    tr = 270.
+    dataDir = '/home/bhoover/UWAOS/WRF_QOIP/data_repository/case_archives/march2020/R_mu/positive/uvTq'
+    # check to make sure dataDir ends in '/', if not, add it to end of string
+    if dataDir[-1] != '/':
+        dataDir = dataDir + '/'
+    # list of initial condition unperturbed files for each iteration
+    unpFileList = glob.glob(dataDir + 'wrfout_d01_unpi*')
+    unpFileList.sort()
+    # list of initial condition perturbed files for each iteration
+    ptdFileList = glob.glob(dataDir + 'wrfout_d01_ptdi*')
+    ptdFileList.sort()
+    # number of files in lists (should all be equal, let's check)
+    if len(unpFileList) != len(ptdFileList):
+        print('LIST MISMATCH: unpFileList size={:d} ptdFileList={:d}'.format(len(unpFileList), len(ptdFileList)))
+        nFiles = None
+    else:
+        print('{:d} files discovered'.format(len(unpFileList)))
+        nFiles = len(unpFileList)
+    # use wrfinput_d01_unpi00 to pick eta values on each level
+    # eta value at each level is computed as etaMidLev, computed as the average between
+    # the eta values at each half-level between [1.,0.]
+    dn = np.asarray(Dataset(dataDir+'wrfout_d01_unpi00').variables['DN']).squeeze()  # d(eta) on half levels
+    eta = 1.0+np.cumsum(dn)
+    # add 0-top level
+    eta = np.append(eta, 0.)
+    # compute eta mid-levels
+    etaMidLev = 0.5*(eta[0:-1] + eta[1:])
+    # loop through files, generating ke and ape on each level
+    nIter = nFiles
+    for i in range(nIter):
+        print('processing iteration {:d} of {:d}'.format(i + 1, nFiles))
+        unpHdl = Dataset(unpFileList[i])
+        ptdHdl = Dataset(ptdFileList[i])
+        if i == 0:
+            lat = np.asarray(unpHdl.variables['XLAT']).squeeze()
+            lon = np.asarray(unpHdl.variables['XLONG']).squeeze()
+            nz = np.shape(np.asarray(unpHdl.variables['P']).squeeze())[0]
+            ke = np.nan * np.ones((nz, nFiles))
+            ape = np.nan * np.ones((nz, nFiles))
+        # vorticity impact for iteration i
+        u0 = np.asarray(unpHdl.variables['U']).squeeze()
+        u1 = np.asarray(ptdHdl.variables['U']).squeeze()
+        v0 = np.asarray(unpHdl.variables['V']).squeeze()
+        v1 = np.asarray(ptdHdl.variables['V']).squeeze()
+        t0 = np.asarray(get_wrf_tk(unpHdl)).squeeze()
+        t1 = np.asarray(get_wrf_tk(ptdHdl)).squeeze()
+        for k in range(nz):
+            up = u1[k,:,:].squeeze() - u0[k,:,:].squeeze()
+            vp = v1[k,:,:].squeeze() - v0[k,:,:].squeeze()
+            tp = t1[k,:,:].squeeze() - t0[k,:,:].squeeze()
+            ke[k,i] = 0.
+            ke[k,i] = ke[k,i] + 0.5*np.sum(up**2.)
+            ke[k,i] = ke[k,i] + 0.5*np.sum(vp**2.)
+            ape[k,i] = 0.
+            ape[k,i] = 0.5*(cp/tr)*np.sum(tp**2.)
+     # 4-panel plot of each iteration's normalized profile for each kinematic term
+    fig,axs=plt.subplots(nrows=2,ncols=2,figsize=(12,12))
+    plot_normalized_iterprofiles(ke, etaMidLev, 'KE', 'gist_rainbow_r', axs.flatten()[0])
+    plot_normalized_iterprofiles(ape, etaMidLev, 'APE', 'gist_rainbow_r', axs.flatten()[1])
+    # save figure
+    #fig.savefig('iterprof_figure.png', bbox_inches='tight', facecolor='white')
+    fig
+    #
+    # end
+    #
+
+
 # In[199]:
 
 
@@ -2839,16 +2965,96 @@ if __name__ == "__main__":
     #
 
 
-# In[ ]:
+# In[78]:
 
 
+def generate_figure_panel(unpInitHdl, ptdInitHdl, unpFcstHdl, ptdFcstHdl):
+    # constants
+    cp = 1004.  # specific heat at constant pressure
+    tr = 270.   # reference temperature
+    L = 2.5104E+06  # latent heat of condensation
+    eps = 1.0  # latent heat coefficient
+    # extract eta-levels from unpInitHdl (should be identical for all files)
+    eta = np.asarray(unpInitHdl.variables['ZNU']).squeeze()  # d(eta) on half levels
+    # extract (u,v,T,q) from each WRF file handle
+    unpInitU = np.asarray(unpInitHdl.variables['U']).squeeze()
+    ptdInitU = np.asarray(ptdInitHdl.variables['U']).squeeze()
+    unpInitV = np.asarray(unpInitHdl.variables['V']).squeeze()
+    ptdInitV = np.asarray(ptdInitHdl.variables['V']).squeeze()
+    unpInitT = np.asarray(get_wrf_tk(unpInitHdl)).squeeze()
+    ptdInitT = np.asarray(get_wrf_tk(ptdInitHdl)).squeeze()
+    unpInitQ = np.asarray(unpInitHdl.variables['QVAPOR']).squeeze()
+    ptdInitQ = np.asarray(ptdInitHdl.variables['QVAPOR']).squeeze()
+    unpFcstU = np.asarray(unpFcstHdl.variables['U']).squeeze()
+    ptdFcstU = np.asarray(ptdFcstHdl.variables['U']).squeeze()
+    unpFcstV = np.asarray(unpFcstHdl.variables['V']).squeeze()
+    ptdFcstV = np.asarray(ptdFcstHdl.variables['V']).squeeze()
+    unpFcstT = np.asarray(get_wrf_tk(unpFcstHdl)).squeeze()
+    ptdFcstT = np.asarray(get_wrf_tk(ptdFcstHdl)).squeeze()
+    unpFcstQ = np.asarray(unpFcstHdl.variables['QVAPOR']).squeeze()
+    ptdFcstQ = np.asarray(ptdFcstHdl.variables['QVAPOR']).squeeze()
+    # compute perturbation quantities
+    pInitU = ptdInitU - unpInitU
+    pInitV = ptdInitV - unpInitV
+    pInitT = ptdInitT - unpInitT
+    pInitQ = ptdInitQ - unpInitQ
+    pFcstU = ptdFcstU - unpFcstU
+    pFcstV = ptdFcstV - unpFcstV
+    pFcstT = ptdFcstT - unpFcstT
+    pFcstQ = ptdFcstQ - unpFcstQ
+    # compute initial and final energy profiles
+    pInitKE = np.nan * np.ones(np.shape(eta))
+    pInitAPE = np.nan * np.ones(np.shape(eta))
+    pInitQE = np.nan * np.ones(np.shape(eta))
+    pFcstKE = np.nan * np.ones(np.shape(eta))
+    pFcstAPE = np.nan * np.ones(np.shape(eta))
+    pFcstQE = np.nan * np.ones(np.shape(eta))
+    for k in range(np.size(eta)):
+        up = pInitU[k,:,:].squeeze()
+        vp = pInitV[k,:,:].squeeze()
+        tp = pInitT[k,:,:].squeeze()
+        qp = pInitQ[k,:,:].squeeze()
+        pInitKE[k] = 0.
+        pInitKE[k] = pInitKE[k] + 0.5 * np.sum(up**2.)
+        pInitKE[k] = pInitKE[k] + 0.5 * np.sum(vp**2.)
+        pInitAPE[k] = 0.
+        pInitAPE[k] = pInitAPE[k] + 0.5 * (cp/tr) * np.sum(tp**2.)
+        pInitQE[k] = 0.
+        pInitQE[k] = pInitKE[k] + 0.5 * eps * L**2./(cp*tr) * np.sum(qp**2.)
+        
+        up = pFcstU[k,:,:].squeeze()
+        vp = pFcstV[k,:,:].squeeze()
+        tp = pFcstT[k,:,:].squeeze()
+        qp = pFcstQ[k,:,:].squeeze()
+        pFcstKE[k] = 0.
+        pFcstKE[k] = pFcstKE[k] + 0.5 * np.sum(up**2.)
+        pFcstKE[k] = pFcstKE[k] + 0.5 * np.sum(vp**2.)
+        pFcstAPE[k] = 0.
+        pFcstAPE[k] = pFcstAPE[k] + 0.5 * (cp/tr) * np.sum(tp**2.)
+        pFcstQE[k] = 0.
+        pFcstQE[k] = pFcstKE[k] + 0.5 * eps * L**2./(cp*tr) * np.sum(qp**2.)
+    return (eta, pInitKE, pInitAPE, pInitQE, pFcstKE, pFcstAPE, pFcstQE)
+    
+    
 
 
-
-# In[ ]:
-
+# In[92]:
 
 
+dataDir = '/home/bhoover/UWAOS/WRF_QOIP/data_repository/case_archives/march2020/R_mu/positive/uvTq'
+unpInitHdl = Dataset(dataDir + '/wrfinput_d01_unpi00')
+ptdInitHdl = Dataset(dataDir + '/wrfinput_d01_ptdi14')
+unpFcstHdl = Dataset(dataDir + '/wrfout_d01_unpi00')
+ptdFcstHdl = Dataset(dataDir + '/wrfout_d01_ptdi14')
+x = generate_figure_panel(unpInitHdl, ptdInitHdl, unpFcstHdl, ptdFcstHdl)
+
+initEng = x[1] + x[2] + x[3]
+fcstEng = x[4] + x[5] + x[6]
+
+plt.plot(20.*initEng,x[0],'b')
+plt.plot(fcstEng,x[0],'r')
+plt.gca().invert_yaxis()
+plt.show()
 
 
 # In[ ]:
