8720969350a5bb58ac63114b2432631beaae1d0f
Brett Hoover <brett.hoover@ssec.wisc.edu>
2024-01-24 16:46:16 -0600

    Modified midlat_paper/figures/FigD.py to remove references to weakened simulations

--- midlat_paper/figures/FigD.py
+++ midlat_paper/figures/FigD.py
@@ -1,10 +1,9 @@
 # Figure D:
-# All panels: Plot the local projection operator for each iteration from the least intense simulation to
-# the most intense simulation, from red to blue respectively, with the LPO of the unperturbed
+# All panels: Plot the local projection operator for each iteration from the unperturbed simulation to
+# the most intense simulation, from black to blue respectively, with the LPO of the unperturbed
 # simulation in black.
-# a) 24-hr forecast sea level pressure of least intense simulation
-# b) 24-hr forecast sea level pressure of unperturbed simulation
-# c) 24-hr forecast sea level pressure of most intense simulation
+# a) 24-hr forecast sea level pressure of unperturbed simulation
+# b) 24-hr forecast sea level pressure of most-intense simulation
 # NOTE: the sea-level pressure minimum can stray slightly from the center of the LPO in the perturbed
 #       simulations, because we are comparing the LPO derived from the *unperturbed* sea-level pressure from
 #       that iteration to the *perturbed* sea-level pressure from the resulting optimal perturbation
@@ -39,30 +38,19 @@ def generate_figure_panel(wrfHdl, sensIndex, figureName):
     fig, ax = plt.subplots(ncols=1,nrows=1,figsize=(14,7), subplot_kw={'projection' : datProj})
     # define contour levels of SLP and thickness
     slpContours = np.arange(900., 1040.1, 4.)
-    # assemble list of final-time sensitivity gradients with respect to MU from least-intense
-    # (largest negative iteration) to most-intense (largest positive iteration)
-    #   NOTE: this assemblage of sensitivity files has two characteristics of note. (1) The
-    #         unperturbed simulation appears twice in the list right next to each other, which
-    #         I am okay with since it just means the same contour is drawn twice without stepping
-    #         on any other contours, (2) a reminder: in operation the response function is multiplied
-    #         by -1, so here we are mapping the least-intense to red and most-intense to blue to match
-    #         the simplified language in the paper where the response function is shown only as the
-    #         average MU in the LPO. These are merely bookkeeping differences.
-    # assemble list of negative (weakening, mapped to red) iterations, and reverse numerical order
-    negSensList = glob('/home/bhoover/UWAOS/WRF_QOIP/data_repository/case_archives/march2020/R_mu/negative/uvTq/final_sens_d01_unpi*')
-    negSensList.sort()
-    negSensList.reverse()
+    # assemble list of final-time sensitivity gradients with respect to MU from unperturbed
+    # to most-intense (largest positive iteration)
+    #   NOTE: in operation the response function is multiplied by -1, so here we are mapping the
+    #         most-intense to blue to match the simplified language in the paper where the response
+    #         function is shown only as the average MU in the LPO. These are merely bookkeeping
+    #         differences.
     # assemble list of positive (intensifying, mapped to blue) iterations
-    posSensList = glob('/home/bhoover/UWAOS/WRF_QOIP/data_repository/case_archives/march2020/R_mu/positive/uvTq/final_sens_d01_unpi*')
-    posSensList.sort()
-    # assemble total list by appending these lists together from list-intense to most-intense, which
-    # double-counts the unperturbed simulation that appears in both experiments
-    sensList = negSensList.copy()
-    senList = sensList.extend(posSensList)
+    sensList = glob('/home/bhoover/UWAOS/WRF_QOIP/data_repository/case_archives/march2020/R_mu/positive/uvTq/final_sens_d01_unpi*')
+    sensList.sort()
     # generate empty lists to store contour values, colors, line-thicknesses, and contour-intervals
     # initialize each list with data for plotting the sea level pressure for wrfHdl
     contVarList = [slp]
-    contColList = ['green']
+    contColList = ['black']
     contThkList = [1.0]
     contIntList = [slpContours]
     # loop through sensList and append each component in-order to the lists
@@ -80,16 +68,6 @@ def generate_figure_panel(wrfHdl, sensIndex, figureName):
         else:
             contColList.append('gray')
             contThkList.append(1.0)
-        # plot the unperturbed simulation(s) in black with double-thickness, otherwise set to an appropriate
-        # value in the red-blue spectrum
-        #if 'final_sens_d01_unpi00' in sensFile:
-        #    contColList.append('black')
-        #else:
-        #    contColList.append(matplotlib.colors.to_hex((1.-i/len(sensList), 0., i/len(sensList))))
-        #if 'final_sens_d01_unpi00' in sensFile:
-        #    contThkList.append(3.0)
-        #else:
-        #    contThkList.append(1.5)
     # generate plan-section plot for figure panel axis
     ax, (shd, cons, vec) = plan_section_plot(wrfHDL=wrfHdl,
                                         lat=lat,
@@ -120,21 +98,11 @@ def generate_figure_panel(wrfHdl, sensIndex, figureName):
 if __name__ == "__main__":
     # define directory of unperturbed, most-intense, and least-intense WRF forecast
     unpDir = '/home/bhoover/UWAOS/WRF_QOIP/data_repository/final_runs/march2020/R_mu/unperturbed/'
-    negDir = '/home/bhoover/UWAOS/WRF_QOIP/data_repository/final_runs/march2020/R_mu/negative/uvTq/ptdi14/'
     posDir = '/home/bhoover/UWAOS/WRF_QOIP/data_repository/final_runs/march2020/R_mu/positive/uvTq/ptdi22/'
     # define datetime of WRF initialization
     dtInit = datetime.datetime(2020, 3, 6, 12)
 
-    # FIG Da: 24-hr least-intense (negative) WRF SLP and all local projection operators
-    fcstHr = 24
-    # define forecast datetime stamp
-    dtFcst = dtInit + datetime.timedelta(hours=fcstHr)
-    dtFcstStr = datetime.datetime.strftime(dtFcst,'%Y-%m-%d_%H:00:00')
-    # define WRF forecast file and open netCDF4 file-handle
-    fileFcst = negDir + 'wrfout_d01_' + dtFcstStr
-    wrfHdl = Dataset(fileFcst)
-    generate_figure_panel(wrfHdl, 0, 'FIGD_panel_A')
-    # FIG Db: 24-hr unperturbed WRF SLP and all local projection operators
+    # FIG Da: 24-hr unperturbed WRF SLP and all local projection operators
     fcstHr = 24
     # define forecast datetime stamp
     dtFcst = dtInit + datetime.timedelta(hours=fcstHr)
@@ -142,8 +110,8 @@ if __name__ == "__main__":
     # define WRF forecast file and open netCDF4 file-handle
     fileFcst = unpDir + 'wrfout_d01_' + dtFcstStr
     wrfHdl = Dataset(fileFcst)
-    generate_figure_panel(wrfHdl, 15, 'FIGD_panel_B')
-    # FIG Dc: 24-hr most intense (positive) WRF SLP and all local projection operators
+    generate_figure_panel(wrfHdl, 0, 'FIGD_panel_A')
+    # FIG Db: 24-hr most-intense WRF SLP and all local projection operators
     fcstHr = 24
     # define forecast datetime stamp
     dtFcst = dtInit + datetime.timedelta(hours=fcstHr)
@@ -151,7 +119,7 @@ if __name__ == "__main__":
     # define WRF forecast file and open netCDF4 file-handle
     fileFcst = posDir + 'wrfout_d01_' + dtFcstStr
     wrfHdl = Dataset(fileFcst)
-    generate_figure_panel(wrfHdl, 37, 'FIGD_panel_C')
+    generate_figure_panel(wrfHdl, 22, 'FIGD_panel_B')
 #
 # end
 #
