804472cfadda92139ba78ec6dbb644ca8b8a7c68
Brett Hoover <brett.hoover@ssec.wisc.edu>
2024-02-13 14:24:42 -0600

    Added experimental compute_inverse_laplacian() function that can use non-zero prescribed boundaries in analysis/analysis_dependencies.py

--- analysis/analysis_dependencies.py
+++ analysis/analysis_dependencies.py
@@ -1826,3 +1826,152 @@ def compute_hires_border(fieldLores, gridHires, i_start, j_start, resRatio):
     # return borderHires
     return borderHires
 
+# compute_inverse_laplacian_with_boundaries: As per compute_inverse_laplacian(), but includes
+#                                            the option to include prescribed non-zero boundary
+#                                            values (still treated as dirichlet BCs, but with
+#                                            spatially varying BCs instead of zeros).
+# INPUTS:
+#   wrfHDL: netCDF4.Dataset() file-handle for WRF file
+#   frc: forcing field (e.g. vorticity for computing streamfunction)
+#   boundaries: None for zero BCs, otherwise grid of np.shape(frc) with prescribed values on
+#               boundary
+#
+# OUTPUTS:
+#   full_psi: field of np.shape(frc) with laplacian field
+#
+# DEPENDENCIES:
+#   numpy
+#   time
+#   wrf
+#   scipy
+#
+def compute_inverse_laplacian_with_boundaries(wrfHDL, frc, boundaries=None):
+    #########################################################################################
+    #
+    # Import necessary modules
+    #
+    import numpy as np #..................................................................... array module
+    from numpy import reshape #.............................................................. numPy array reshape routine
+    import time #............................................................................ clock module
+    from wrf import to_np #.................................................................. xarray-to-numPy-array conversion routine
+    import scipy as sp #..................................................................... scientific function module
+    import scipy.optimize #.................................................................. sciPy function optimization routine
+    import scipy.linalg #.................................................................... sciPy linear algebra routine
+    import scipy.sparse #.................................................................... sciPy sparse array routine
+    #
+    #########################################################################################
+    #
+    # Initialize forcing (-kin) field, and interior and boundary conditions of solved (psi)
+    # field. psi will become full_psi once it has converged.
+    #
+    # Start clock
+    start = time.time() #.................................................................... beginning time
+    #
+    # FIELDS DEFINED
+    #
+    # Define 2D grid-dimensions
+    ny,nx = np.shape(frc) #.................................................................. 2D grid-dimensions, from forcing grid
+    # Define grid map-factors
+    msfm = np.asarray(wrfHDL.variables['MAPFAC_M']).squeeze() #.............................. grid map factors on mass points
+    # Define grid-spacing in x- and y-directions
+    ds_x = wrfHDL.DX #....................................................................... grid dx
+    ds_y = wrfHDL.DY #....................................................................... grid dy
+    # Define inverted field: psi
+    if boundaries is not None:
+        psi = np.copy(boundaries)
+        psib = np.copy(boundaries)
+    else:
+        psi = np.zeros((ny, nx)) #............................................................... initialized psi (Dirichlet boundary conditions)
+        psib = np.copy(psi) #............................................................................. prescribed psi boundary value (Dirichlet boundary conditions)
+    # Define full_psi, the psi array that is operated on in relaxation
+    full_psi=np.zeros((ny,nx)) #............................................................. initialized full_psi
+    # Define dimensions of interior-points to grid
+    nxm2 = nx - 2 #.......................................................................... x-direction inner points number
+    nym2 = ny - 2 #.......................................................................... y-direction inner points number
+    # Define forcing field and forcing field as a flattened vector
+    forcegrid=np.ndarray(shape=(nym2,nxm2)) #................................................ forcing grid (interior points only, initialized empty)
+    forcevec=np.ndarray(forcegrid.size) #.................................................... forcing vector of forcegrid
+    #
+    # FIELDS INITIALIZED
+    #
+    # Initialize forcegrid, forcevec, and psi
+    psi=full_psi[:,:] #...................................................................... psi (initialized to full_psi initial values)
+    forcegrid = frc[1:ny-1,1:nx-1]/msfm[1:ny-1,1:nx-1]**2.0 #................................ forcegrid (initialized to frc, divided by square of map-factors)
+    forcevec[:]=to_np(forcegrid).flatten() #................................................. forcevec (initialized to forcegrid as flattened array)
+    # Initialize boundary conditions of forcevec
+    for i in range(nym2):
+        for j in range(nxm2):
+            index=i*nxm2+j #................................................................. TEMPORARY VARIABLE: index of current [i,j] point in forcevec
+            # Adjust values in forcevec based on prescribed boundary conditions
+            #if(i==0) : forcevec[index] = forcevec[index] - psib[i,j-2]/ds_x**2.
+            #if(i==nxm2-1): forcevec[index] = forcevec[index] - psib[i,j+2]/ds_x**2.
+            #if(j==0): forcevec[index] = forcevec[index] - psib[i-2,j]/ds_y**2
+            #if(j==nym2-1): forcevec[index] = forcevec[index] - psib[i+2,j]/ds_y**2.
+            if(i==0) : forcevec[index] = forcevec[index] - psib[i,j]/ds_x**2.
+            if(i==nym2-1): forcevec[index] = forcevec[index] - psib[i+2,j]/ds_x**2.
+            if(j==0): forcevec[index] = forcevec[index] - psib[i,j]/ds_y**2
+            if(j==nxm2-1): forcevec[index] = forcevec[index] - psib[i,j+2]/ds_y**2.
+    #
+    #########################################################################################
+    #
+    # Compute inverse-laplacian of forcevec to recover psi
+    #
+    # Define laplacian operators
+    lap = np.zeros((nxm2*nym2,nxm2*nym2),dtype=np.float64) #................................. laplacian operators field
+    # Initialize laplacian operator values
+    for i in range(nym2):
+        for j in range(nxm2):
+            index=i*nxm2 + j #............................................................... TEMPORARY VARIABLE: index of current [i,j] point in forcevec
+            # Construct indices of 5-point molecule around index
+            A1 = (i-1)*nxm2 + j #............................................................ TEMPORARY VARIABLE: south point
+            A2 = i*nxm2 + (j-1) #............................................................ TEMPORARY VARIABLE: west point
+            A3 = index #..................................................................... TEMPORARY VARIABLE: center [i,j] point
+            A4 = i*nxm2 + (j+1) #............................................................ TEMPORARY VARIABLE: east point
+            A5 = (i+1)*nxm2 + j #............................................................ TEMPORARY VARIABLE: north point
+            # Compute laplacian for points in molecule
+            if((i>0)):lap[index,A1] = 1./ds_y**2;
+            if(j<nxm2-1): lap[index,A4] = 1./ds_y**2;
+            if(j>0) :lap[index,A2] = 1./ds_x**2;
+            if(i<nym2-1):lap[index,A5] = 1./ds_x**2;
+            if((i>=0) and (i<=nym2) and (j>=0) and (j<=nxm2)): lap[index,index]=-2./ds_x**2.-2./ds_y**2.
+    # Apply block-sparse-row function to lap
+    lap = scipy.sparse.bsr_matrix(lap)
+    # Compute transpose (adjoint) of laplacian operator
+    laps = lap.T #........................................................................... transpose of lap
+    # Define initial guess of psi
+    psi0 = np.ones((nym2, nxm2), dtype=np.float64) #......................................... initial guess of psi (all ones)
+    # Define laplacian and transpose of laplacian operators
+    LAP = lap
+    LAPS = laps
+    #
+    # Define functions to solve
+    #
+    # L-BFGS method
+    #
+    # Define a function to compute error term
+    def fun(psi0):
+        error = LAP.dot(psi0) - forcevec
+        return 0.5*(error**2).sum()
+    # Define a function to compute gradient term
+    def grad(psi0):
+        return LAPS.dot(LAP.dot(psi0) - forcevec)
+    # Optimize to solve
+    field, value, info = scipy.optimize.fmin_l_bfgs_b(fun, psi0.flatten(), grad, pgtol=1e-16, factr=.0001,
+                                                  maxiter=150000,maxfun=15000*50)
+    # Stop clock
+    end = time.time() #...................................................................... Ending time
+    #
+    #########################################################################################
+    #
+    # Report timing statistics for solving on level-k
+    print('time for solve = ', end - start, ' seconds.' )
+    print(info)
+    # Define full_psi as reshaped matrix of converged psi
+    full_psi[:,:] = psi.copy()
+    full_psi[1:-1,1:-1] = reshape(field, (nym2, nxm2)).copy()
+    # Return full_psi
+    return full_psi[:,:]
+    #
+    # END
+    #
+
